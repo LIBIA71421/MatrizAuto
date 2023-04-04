02cca6c4261f65d9fbe72ac7381051d033a8ee51
libia <libia.roman@ucb.edu.bo>
2023-04-04 13:00:09 +0200

    deploy

--- src/presenter.js
+++ src/presenter.js
@@ -18,13 +18,4 @@ form.addEventListener("submit", (event) => {
     }
     return matriz;
   }
-  var tabla = document.createElement("table");
-  for (var i = 0; i < miMatriz.length; i++) {
-    var fila = tabla.insertRow();
-    for (var j = 0; j < miMatriz[i].length; j++) {
-      var celda = fila.insertCell();
-      celda.appendChild(document.createTextNode(miMatriz[i][j]));
-    }
-  }
-  tablaDiv.appendChild(tabla);
 });
