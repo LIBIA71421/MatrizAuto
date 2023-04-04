4f0b65c1f1bc06253a7f8d28716113b660778bbb
libia <libia.roman@ucb.edu.bo>
2023-04-04 12:19:06 +0200

    Matriz js

---
+++ src/ControlAuto.js
@@ -0,0 +1,13 @@
+function Matriz(filas, columnas) {
+    var matriz = [];
+    for (var i = 0; i < filas; i++) {
+      matriz[i] = [];
+      for (var j = 0; j < columnas; j++) {
+
+        matriz[i][j] = "0"; 
+      }
+    }
+    return matriz;
+  }
+
+  export default Matriz;
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,6 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+import Matriz from "./ControlAuto.js";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  
 });
